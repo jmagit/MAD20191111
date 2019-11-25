6d73e2f84bd6e9fd46838d51360177d92c36cee7
Javier <jmartin@grupoloyal.com>
2019-11-25 14:17:48 +0100

    Model

---
+++ Nuevo/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Nuevo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nuevo.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Kk() {
+            AWEntities db = new AWEntities();
+            var list = db.Customers.Where(p => p.FirstName.StartsWith("S"))
+                    .OrderBy(p=> p.LastName)
+                    .Skip(5)
+                    .Take(5)
+                    .ToList();
+            return View();
+        }
+    }
+}
