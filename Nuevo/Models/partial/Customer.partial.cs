da208d66072f21ae82e4571210725516f89a952f
Javier <jmartin@grupoloyal.com>
2019-11-28 10:02:00 +0100

    Partial

---
+++ Nuevo/Models/partial/Customer.partial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nuevo.Models {
+    public partial class Customer {
+        public string NombreCompleto {
+            get {
+                return LastName + ", " + FirstName;
+            }
+        }
+    }
+}
